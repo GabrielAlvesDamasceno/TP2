98d8b78444151beb1770948f6216e6bd64dd5ef0
GabrielAlvesDamasceno <gabrieldamasceno2008@outlook.com>
2023-11-20 16:53:38 -0300

    modificações

--- tp2.js
+++ tp2.js
@@ -32,3 +32,39 @@ $bSalvar.click(function () {
 $bCarregar.click(function () {
     $body.css('background-color', localStorage.getItem('cor'));
 });
+
+cheet('l a r a n j a', function () {
+    $body.css('background-color', 'orange');
+});
+
+cheet('v e r m e l h o', function () {
+    $body.css('background-color', 'red');
+});
+
+cheet('p r e t o', function () {
+    $body.css('background-color', 'black');
+});
+
+cheet('v i a d o', function () {
+    $body.css('background-color', 'hotpink');
+});
+
+cheet('p r e t o', function () {
+    $body.css('background-color', 'black');
+});
+
+cheet('a z u l', function () {
+    $body.css('background-color', 'blue');
+});
+
+cheet('v e r d e', function () {
+    $body.css('background-color', 'green');
+});
+
+cheet('b r a n c o', function () {
+    $body.css('background-color', 'white');
+});
+
+cheet('a m a r e l o', function () {
+    $body.css('background-color', 'yellow');
+});
